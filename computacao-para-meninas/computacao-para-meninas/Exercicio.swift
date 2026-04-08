b3150f797f2babf39efcb2f6b3bc887905c0766f
Ana Margarida Borges <a252137@dac.unicamp.br>
2026-04-08 08:30:53 -0300

    fix bug of not filling full screen

--- computacao-para-meninas/computacao-para-meninas/Exercicio.swift
+++ computacao-para-meninas/computacao-para-meninas/Exercicio.swift
@@ -7,6 +7,30 @@
 
 enum TipoExercicio: Decodable {
     case tipo3([Int], [Int])
+
+    enum CodingKeys: String, CodingKey {
+        case tipo
+        case valores1
+        case valores2
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let tipo = try container.decode(String.self, forKey: .tipo)
+
+        switch tipo {
+        case "tipo3":
+            let v1 = try container.decode([Int].self, forKey: .valores1)
+            let v2 = try container.decode([Int].self, forKey: .valores2)
+            self = .tipo3(v1, v2)
+        default:
+            throw DecodingError.dataCorruptedError(
+                forKey: .tipo,
+                in: container,
+                debugDescription: "Tipo desconhecido: \(tipo)"
+            )
+        }
+    }
 }
 
 struct Exercicio: Decodable {
